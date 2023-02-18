49f6d3e3eb8a029aa486c289a4f01f22c702fce7
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-02-18 15:01:27 +0100

    basic pot reading, something weird, values too high

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ or use the script:
 ```bash
 ./displayOutput
 ```
+
+exit `minicom` with `Ctrl-A` then `X`
+
+## Compile and upload
+
+This command will compile and upload the code to the pico,
+it will ask you to unplug it and re-plug it while holding the `BOOTSEL` button.
+
+```bash
+./run
+```
--- main.cpp
+++ main.cpp
@@ -2,13 +2,34 @@
 // https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf#page=15
 
 #include <stdio.h>
+
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+constexpr int POT_0 = 26;
+constexpr int POT_1 = 27;
 
 int main() {
     stdio_init_all();
+    printf("PICOLIGHT!\n");
+
+    adc_init();
+
+    adc_gpio_init(POT_0);
+    adc_gpio_init(POT_1);
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        adc_select_input(0);
+        uint16_t pot_0 = adc_read();
+
+        adc_select_input(1);
+        uint16_t pot_1 = adc_read();
+
+        printf("POT_0: %04d, POT_1: %04d\n", pot_0, pot_1);
+
+        sleep_ms(200);
     }
+
     return 0;
 }
