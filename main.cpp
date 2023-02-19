fdf76b0b9d7df379286b70f86fda761dcf8691ef
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-02-19 19:36:23 +0100

    moving average

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf#page=15
 
 #include <cmath>
-
+#include <deque>
 #include <stdio.h>
 
 #include "pico/stdlib.h"
@@ -17,6 +17,38 @@ constexpr float BATTERY_V_DIVIDER = 25.4375;
 uint32_t time_ms() {
     return to_ms_since_boot(get_absolute_time());
 }
+
+class MovingAverage {
+    float_t sum = 0;
+    float_t average = 0;
+    uint32_t samples_wanted = 0;
+    std::deque<float_t> samples;
+
+    public:
+        MovingAverage(int samples_wanted) : samples_wanted(samples_wanted) {
+
+        }
+
+        void add(float_t value) {
+            samples.push_back(value);
+            sum += value;
+
+            if (samples.size() > samples_wanted) {
+                sum -= samples.front();
+                samples.pop_front();
+            }
+
+            average = sum / samples.size();
+        }
+
+        bool ready() {
+            return samples.size() >= samples_wanted;
+        }
+
+        float_t get() {
+            return average;
+        }
+};
 
 int main() {
     stdio_init_all();
@@ -34,6 +66,8 @@ int main() {
     auto last = start;
     uint64_t loops = 0;
 
+    MovingAverage avg_battery_v(1000);
+
     while (true) {
         loops++;
 
@@ -48,22 +82,23 @@ int main() {
 
         adc_select_input(2);
         uint16_t battery = adc_read();
-        float battery_v_divided = battery * 3.3 / 4095;
-        float battery_v = battery_v_divided * BATTERY_V_DIVIDER;
+        float_t battery_v_divided = battery * 3.3 / 4095;
+        float_t battery_v = battery_v_divided * BATTERY_V_DIVIDER;
+        avg_battery_v.add(battery_v);
 
         auto now = time_ms();
-        if (now > last + 500) {
+        if (now > last + 200) {
             int loops_per_second = loops * 1000 / (now - start);
             printf("dt: %d, loops / s: %d\n", now - last, loops_per_second);
 
             printf(
-                "coarse: %04d, fine: %04d, level: %05d, pct: %f\n",
+                "coarse: %04d, fine: %04d, level: %05d, pct: %1.5f\n",
                 coarse, fine, level, pct
             );
 
             printf(
-                "battery: %04d, battery_v_divided: %1.5f, battery_v: %2.5f\n\n",
-                battery, battery_v_divided, battery_v
+                "battery: %04d, battery_v_divided: %1.5f, avg_battery_v: %2.5f\n\n",
+                battery, battery_v_divided, avg_battery_v.get()
             );
 
             last = now;
