163f29fd38f52d9e1424d3bca68e17cf155a33f3
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2023-05-08 16:56:05 +0200

    under voltage protection toggle

--- main.cpp
+++ main.cpp
@@ -35,6 +35,29 @@ static float pwm_frequency;
 constexpr float adj_steps = 15;
 constexpr float level_max = (adj_steps - 1) * (adj_steps + 1);
 
+static char event_str[128];
+
+void gpio_event_string(char *buf, uint32_t events);
+
+static bool under_voltage_protection_enabled = true;
+
+void btn_0_gpio_cb(uint gpio, uint32_t events) {
+    // Put the GPIO event(s) that just happened into event_str
+    // so we can print it
+    gpio_event_string(event_str, events);
+    printf("GPIO %d %s\n", gpio, event_str);
+
+    if (events & GPIO_IRQ_EDGE_RISE) {
+        if (under_voltage_protection_enabled) {
+            printf("disabling under voltage protection\n");
+            under_voltage_protection_enabled = false;
+        } else {
+            printf("enabling under voltage protection\n");
+            under_voltage_protection_enabled = true;
+        }
+    }
+}
+
 uint32_t time_ms() {
     return to_ms_since_boot(get_absolute_time());
 }
@@ -97,6 +120,34 @@ class MovingAverage {
         }
 };
 
+static const char *gpio_irq_str[] = {
+        "LEVEL_LOW",  // 0x1
+        "LEVEL_HIGH", // 0x2
+        "EDGE_FALL",  // 0x4
+        "EDGE_RISE"   // 0x8
+};
+
+void gpio_event_string(char *buf, uint32_t events) {
+    for (uint i = 0; i < 4; i++) {
+        uint mask = (1 << i);
+        if (events & mask) {
+            // Copy this event string into the user string
+            const char *event_str = gpio_irq_str[i];
+            while (*event_str != '\0') {
+                *buf++ = *event_str++;
+            }
+            events &= ~mask;
+
+            // If more events add ", "
+            if (events) {
+                *buf++ = ',';
+                *buf++ = ' ';
+            }
+        }
+    }
+    *buf++ = '\0';
+}
+
 int main() {
     stdio_init_all();
     printf("PICOLIGHT!\n");
@@ -108,6 +159,7 @@ int main() {
     adc_gpio_init(BATTERY_PIN);
 
     gpio_init(BTN_0);
+    gpio_set_irq_enabled_with_callback(BTN_0, GPIO_IRQ_EDGE_RISE, true, &btn_0_gpio_cb);
     gpio_init(LED_0);
     gpio_set_dir(BTN_0, GPIO_IN);
     gpio_set_dir(LED_0, GPIO_OUT);
@@ -122,8 +174,7 @@ int main() {
     MovingAverage coarse_avg(100);
     MovingAverage fine_avg(100);
 
-    bool voltage_limiter = true;
-    gpio_put(LED_0, voltage_limiter);
+    gpio_put(LED_0, under_voltage_protection_enabled);
 
     while (true) {
         loops++;
@@ -152,8 +203,16 @@ int main() {
 
         float duty_cycle = pct;
 
+        bool light_on = true;
+
+        gpio_put(LED_0, under_voltage_protection_enabled);
+
+        if (under_voltage_protection_enabled && battery_pct < 5) {
+            light_on = false;
+        }
+
         // under-voltage protection
-        if (battery_pct >= 1) {
+        if (light_on) {
             set_duty_cycle(PWM_PIN, duty_cycle);
         } else {
             set_duty_cycle(PWM_PIN, 0);
